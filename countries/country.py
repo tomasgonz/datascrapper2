4677004f71f7a393d03ea6597ca54881f9977a83
Tomás <tomasgonz@gmail.com>
2019-07-07 21:36:30 -0400

    Fixed bug in as_json to reflect change in variable name

--- countries/country.py
+++ countries/country.py
@@ -18,7 +18,7 @@ class Country:
         return ("*****\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s" % (self.name, self.aliases, self.iso2code, self.capital, self.iso3code, self.incomelevel, self.capital_longitude, self.capital_latitude, self.lendingtype, self.region, str(self.groups), str(self.alias), self.fao_code, self.sdg_code))
     def as_json(self):
     	# The replacement of (.) is done to avoid issues with key in mongodb
-        return dict(name=self.name.replace(".", ""), aliases=self.aliases, iso2code=self.iso2code, capitalcity=self.capitalcity, iso3code=self.iso3code, incomelevel=self.incomelevel, longitude=self.longitude, latitude=self.latitude, lendingtype=self.lendingtype, region=self.region, groups=self.groups)
+        return dict(name=self.name.replace(".", ""), aliases=self.aliases, iso2code=self.iso2code, capitalcity=self.capitalcity, iso3code=self.iso3code, incomelevel=self.incomelevel, capital_longitude=self.longitude, capital_latitude=self.latitude, lendingtype=self.lendingtype, region=self.region, groups=self.groups)
 
     def as_array(self):
         return([self.name, str(self.aliases), self.iso2code, self.capital, self.iso3code, self.incomelevel, str(self.capital_longitude), str(self.capital_latitude), self.lendingtype, self.region, str(self.groups), str(self.alias), self.fao_code, self.sdg_code])
