0900ed5e3f3cd6b4c1415814d4d8df1189498587
Tomas Gonzalez <tomasgonz@gmail.com>
2019-10-28 17:09:57 -0400

    Added ability to query for country group to the population source

--- sources/unpd/indicators.py
+++ sources/unpd/indicators.py
@@ -9,9 +9,13 @@ c = CountryList()
 c.load_wb()
 cache_folder = os.getcwd().split('datascrapper2')[0] + 'datascrapper2/sources/unpd/data'
 
-def load_projections():
-    df = pd.read_excel(cache_folder + "/UN_PPP2019_PopTot.xlsx")
-    df = df.rename(columns={'Region, subregion, country or area': 'entity'})
+def load_projections(dataset = 'all'):
+    
+    if dataset == 'all':
+        df = pd.read_excel(cache_folder + "/UN_PPP2019_PopTot.xlsx")    
+    elif dataset == '0-24':
+        df = pd.read_excel(cache_folder + "/UN_PPP2019_Output_Pop0-24.xlsx")    
+
     tdf = Frame.Frame()
     cols = df.columns.values
     for i in range(0,len(df)):
@@ -61,30 +65,30 @@ def get_age_group_by_country_groups(age_group, country_groups):
         datasets.append([x, sums, g, ''])
     return datasets
 
-def get_population_projection(ctrs):
+def get_population_projection(ctrs, dataset='all'):
     new_df = Frame.Frame()
-    data_frame = load_projections()
+    data_frame = load_projections(dataset=dataset)
     for r in data_frame:
         if (r.get_by_column_name('entity').get_value() in ctrs):
             new_df.rows.append(r)    
     return new_df
 
-def get_projection_by_group(group):    
+def get_projection_by_group(group, dataset='all'):    
     ctrs = c.get_country_names_and_aliases(c.get_countries_in_group([group]))    
     new_df = Frame.Frame()
-    data_frame = load_projections()
+    data_frame = load_projections(dataset=dataset)
     for r in data_frame:        
         if (r.get_by_column_name('entity').get_value() in ctrs):
             new_df.rows.append(r)    
     
     return new_df
 
-def get_projection_by_groups(groups):
+def get_projection_by_groups(groups, dataset='all'):
     datasets = []
     x = get_projection_by_group(groups[0]).get_column_names()[2:len(get_projection_by_group(groups[0]).get_column_names())]
     for g in groups:    
         y = get_projection_by_group(g).get_total_by_column()[2:len(get_projection_by_group(g).get_column_names())]
-        datasets.append([x, y, g, 'Projected population, thousands'])
+        datasets.append([x, y, g, 'Projected population, ' + dataset + ', thousands'])
     
     return datasets
 
