3af12f2c1708354f44ecde51cfdf7a281c7d3d02
Tomas <tomasgonz@gmail.com>
2022-09-20 17:37:24 -0400

    Work on OECDdata source

--- sources/oecd/oda.py
+++ sources/oecd/oda.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import os
+from geo.groups import *
+c = Countries()
+c.load_wb()
 
 data = pd.DataFrame()
 
 def load_data():
     global data
     data = pd.read_csv(os.path.join(os.path.dirname(__file__), "table2aoecd.csv"))
+
+# Get's the aggregate of a group of countries, such as LDCs, UMICs, etc.
+def get_group_agg(indicators, years, donors, group):
+    ctrs = c.get_group(group).get_names()
+    
+    filtered_data = data[data['Recipient'].isin(ctrs)]
+    filtered_data1 = filtered_data[filtered_data['Donor'].isin(donors)]
+    filtered_data2 = filtered_data1[filtered_data1['Year'].isin(years)]
+    filtered_data3 = filtered_data2[filtered_data2['Aid type'].isin(indicators)]
+    
+    grouped_data = filtered_data3.groupby('Year', as_index=False).sum()
+    grouped_data['Recipient'] = group
+
+    return grouped_data
 
 def get_data(indicators, years, donors, recipients):
     global data
@@ -16,6 +33,13 @@ def get_data(indicators, years, donors, recipients):
     filtered_data1 = filtered_data[filtered_data['Donor'].isin(donors)]
     filtered_data2 = filtered_data1[filtered_data1['Year'].isin(years)]
     filtered_data3 = filtered_data2[filtered_data2['Aid type'].isin(indicators)]
+
+    groups = ['LDCs', 'LLDCs' 'SIDS']
+
+    for element in groups:
+        if element in recipients:
+           filtered_data3 = pd.concat([filtered_data3, 
+           get_group_agg(indicators, years, donors, element)])
 
     return filtered_data3
 
@@ -33,6 +57,13 @@ def get_donors():
         load_data()
     donors = data['Donor'].unique()
     return donors
+
+def get_recipients():
+    global data
+    if data.empty:  
+        load_data()
+    recipients = data['Recipient'].unique()
+    return recipients
 
 def get_years():
     global data
--- streamlit/oda.py
+++ streamlit/oda.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append("../")
 import streamlit as st
 import pandas as pd
+import numpy as np
+import altair as alt
 
 from sources import oecd
 from geo import *
@@ -14,25 +16,32 @@ ldcs = c.get_group('LDCs').get_names()
 odcs = c.get_group('Developing excluding LDCs').get_names()
 
 recipient_groups = ['LDCs', 'LLDCs', 'SIDS', 'Other developing countries']
+all_recipient_groups = oecd.oda.get_recipients().tolist() + recipient_groups
 
-selected_recipients = st.sidebar.multiselect('Recipient groups', ldcs)
+# recipient_groups = ['LDCs', 'LLDCs', 'SIDS', 'Other developing countries'] + oecd.oda.get_recipients()
 
-selected_donors = st.sidebar.multiselect('Donors', oecd.oda.get_donors())
+selected_recipient = st.sidebar.multiselect('Recipient groups', all_recipient_groups)
 
-selected_aid_types = st.sidebar.multiselect('Aid types', oecd.oda.get_aid_type())
+selected_donor = st.sidebar.selectbox('Donors', oecd.oda.get_donors())
 
-selected_years = st.sidebar.multiselect('Years', oecd.oda.get_years())
+selected_aid_type = st.sidebar.selectbox('Aid types', oecd.oda.get_aid_type())
 
-data = oecd.oda.get_data(selected_aid_types, selected_years, selected_donors, selected_recipients)
+data = oecd.oda.get_data([selected_aid_type], oecd.oda.get_years(), [selected_donor], selected_recipient)
 
 show_data_table = st.sidebar.checkbox("Show data table")
 
 if show_data_table:
-    data
+    st.dataframe(data)
 
 chart_data = data[['Recipient', 'Year', 'Value']]
 
-st.line_chart(chart_data, x ='Recipient', y='Value')
+if not chart_data.empty:
+    line = alt.Chart(chart_data).mark_line().encode( x = 'Year', y='Value', color='Recipient')
+    st.altair_chart(line, use_container_width=True)
+    chart_data_wide = chart_data[['Recipient', 'Year', 'Value']].pivot_table(index='Recipient', columns='Year', values = 'Value')
+    chart_data_wide
+    #chart_data.pivot(chart_data, index='Recipient', columns='Year')['Value']
 
-
-
+st.write("Destination of Official Development Assistance Disbursements. Geographical breakdown by donor, recipient and for some types of aid (e.g. grant, loan, technical co-operation) on a disbursement basis (i.e. actual expenditures). The data cover flows from all bilateral and multilateral donors except for Tables DAC 1, DAC 4, DAC 5 and DAC 7b which focus on flows from DAC member countries and the EU Institutions.")
+st.write("8 April 2022. Next update: June 2022")
+st.write("Official Development Assistance (ODA) is defined as those flows to developing countries and multilateral institutions provided by official agencies, including state and local governments, or by their executive agencies, each transaction of which meets the following tests: i) it is administered with the promotion of the economic development and welfare of developing countries as its main objective; and ii) it is concessional in character and conveys a grant element of at least 25 per cent.")
