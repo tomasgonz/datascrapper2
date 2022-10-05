e68430bcbf675fb4291f400682438c65611af09f
Tomas <tomasgonz@gmail.com>
2022-10-05 16:56:56 -0400

    Added ODA by destination

--- streamlit/oda_sectors.py
+++ streamlit/oda_sectors.py
@@ -25,12 +25,21 @@ data = data[data['SECTOR'].isin(sectors)]
 data = data[data['Donor'] != 'Official Donors, Total']
 data = data[data['Donor'] != 'DAC Countries, Total']
 
+data = data[~data['Recipient'].str.contains('Total')]
+data = data[~data['Recipient'].str.contains('regional')]
+data = data[~data['Recipient'].str.contains('unspecified')]
+
 all_sectors = data['Sector'].unique()
 all_donors = data['Donor'].unique()
+all_recipients = data['Recipient'].unique()
+
+all_donors = np.insert(all_donors, 0, "All")
 
 selected_donor = st.sidebar.selectbox('Donor', all_donors)
+
 if selected_donor:
-    data = data[data['Donor'].isin([selected_donor])]
+    if selected_donor != "All":
+        data = data[data['Donor'].isin([selected_donor])]
 
 # 2020
 data_donors_2020 = data[data['Year'] == 2020]
@@ -47,10 +56,10 @@ for e in data_donors_2020['Sector'].values.tolist():
     index_destination.append(labels.index(e))
 
 st.vega_lite_chart(data, {
-    'mark': {'type': 'line', 'tooltip': True},
+    'mark': {'type': 'line', 'tooltip': True, "interpolate": "monotone", "point": "True"},
     'encoding': {
         'x': {'field': 'Year', 'type' : 'ordinal' },
-        'y': {'field': 'Value', 'type': 'quantitative', 'aggregate':'median'},
+        'y': {'field': 'Value', 'type': 'quantitative', 'aggregate':'sum'},
         'color': {'field': 'Sector', 'type': 'nominal'},
     },
 }, use_container_width=True, height=800)
@@ -77,4 +86,38 @@ fig.update_layout(height=1000)
 
 st.plotly_chart(fig, font_size=10, use_container_width=True, height=1000)
 
-data_donors_2020
+st.header("Where are donors sending their aid in 2020?")
+
+# 2020
+data_donors_2020 = data[data['Year'] == 2020]
+data_donors_2020 = data.groupby(['Donor', 'Recipient'], as_index = False).sum()
+
+labels =  data_donors_2020['Donor'].unique().tolist() + all_recipients.tolist()
+
+index_source = []
+for e in data_donors_2020['Donor'].values.tolist():
+    index_source.append(labels.index(e))
+
+index_destination = []
+for e in data_donors_2020['Recipient'].values.tolist():
+    index_destination.append(labels.index(e))
+
+fig = go.Figure(data=[go.Sankey(
+    node = {
+        'pad' : 15,
+        'thickness' : 20,
+        'line' : {
+            'color' : 'black', 
+            'width' : 0.5
+        },
+        'label' :labels
+    },
+    link = {
+        'source' : index_source,
+        'target' : index_destination,
+        'value' : data_donors_2020['Value']
+    }
+)])
+
+fig.update_layout(height=1000)
+st.plotly_chart(fig, font_size=10, use_container_width=True, height=1000)
